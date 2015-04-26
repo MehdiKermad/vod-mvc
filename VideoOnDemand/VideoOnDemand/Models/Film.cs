ea359fde2d20eba06ec55386324a30fc88a27996
Mehdi93 <mehdi.kermad@y-nov.com>
2015-04-26 17:37:33 +0200

    creation classes Film et Utilisateur

---
+++ VideoOnDemand/VideoOnDemand/Models/Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoOnDemand.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public String Nom { get; set; }
+        public String Realisateur { get; set; }
+        public String Theme { get; set; }
+        public DateTime Sortie { get; set; }
+
+    }
+}
---
+++ VideoOnDemand/VideoOnDemand/Models/Utilisateur.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoOnDemand.Models
+{
+    public class Utilisateur
+    {
+        public int Id { get; set; }
+        public String Pseudo { get; set; }
+        public String Mdp { get; set; }
+        public bool Admin { get; set; }
+    }
+}
