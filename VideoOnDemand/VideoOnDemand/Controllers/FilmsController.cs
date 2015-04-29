81276548ccd22746f7a27db39b87462f44492a17
Mehdi93 <mehdi.kermad@y-nov.com>
2015-04-29 15:26:45 +0200

    affichage film en liste + ajout images

---
+++ VideoOnDemand/VideoOnDemand/Controllers/FilmsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VideoOnDemand.DAL;
+using VideoOnDemand.Models;
+
+namespace VideoOnDemand.Controllers
+{
+    public class FilmsController : Controller
+    {
+        private VODContext db = new VODContext();
+
+        // GET: Films
+        public ActionResult Index()
+        {
+            var films = db.Films.OrderBy(film => film.Ajout);
+            films.Reverse();
+
+            return View(films);
+        }
+
+        // GET: Films/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Film film = db.Films.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            return View(film);
+        }
+
+        // GET: Films/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Films/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Theme,Description,Sortie,Ajout")] Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Films.Add(film);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(film);
+        }
+
+        // GET: Films/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Film film = db.Films.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            return View(film);
+        }
+
+        // POST: Films/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Theme,Description,Sortie,Ajout")] Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(film).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(film);
+        }
+
+        // GET: Films/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Film film = db.Films.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            return View(film);
+        }
+
+        // POST: Films/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Film film = db.Films.Find(id);
+            db.Films.Remove(film);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ VideoOnDemand/VideoOnDemand/DAL/VODContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using VideoOnDemand.Models;
+
+namespace VideoOnDemand.DAL
+{
+    public class VODContext : DbContext
+    {
+        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Director> Directors { get; set; }
+    }
+}
