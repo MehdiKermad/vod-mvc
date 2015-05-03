5f8a485285a7b403c2073bd8a5915f4bada99ef5
Mehdi93 <mehdi.kermad@y-nov.com>
2015-05-03 23:35:48 +0200

    ajout fichiers

---
+++ VideoOnDemand/VideoOnDemand/ModelViews/SearchViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VideoOnDemand.DAL;
+using VideoOnDemand.Models;
+
+namespace VideoOnDemand.ModelViews
+{
+    public class SearchViewModel
+    {
+        public string Theme { get; set; }
+
+        public SearchViewModel() { }
+
+        public SearchViewModel(string Theme)
+        {
+            this.Theme = Theme;
+        }
+
+    }
+}
